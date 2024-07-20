8004ec66d44a046ab3af1eae7b16284aa2dcc80f
Tran Duyen <duyentran8006@gmail.com>
2024-07-20 23:02:43 +0700

    api getUserById

---
+++ BACK_END/VTI-Food/src/main/java/VTIFOOD/VTI_Food/DTO/RoleDto.java
@@ -0,0 +1,12 @@
+package VTIFOOD.VTI_Food.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoleDto {
+    private Long id;
+    private String role;
+    private String name;
+}
